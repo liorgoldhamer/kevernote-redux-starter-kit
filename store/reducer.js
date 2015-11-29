f88740e20eeaa8b7ebd4cdd1f8f6bf24bfe59a0c
Jonathan Avinor <jonathan.avinor@klarna.com>
2015-11-30 00:38:26 +0200

    wrapping cases with curly braces

--- store/reducer.js
+++ store/reducer.js
@@ -1,28 +1,28 @@
 export default function reducer(state, action) {
   switch (action.type) {
-  case 'NOTE_SELECTED':
-    let selected = state.notes.filter(note => note.id === action.id)[0]
-    return {...state, selected: selected}
+    case 'NOTE_SELECTED': {
+      let selected = state.notes.filter(note => note.id === action.id)[0]
+      return {...state, selected: selected}
+    }
+    case 'NOTE_BODY_CHANGED': {
+      let newNotes = Array.from(state.notes, (note) => {
+        return (note.id !== action.id)
+          ? note
+          : {...note, body: action.newBody}
+      })
 
-  case 'NOTE_BODY_CHANGED': {
-    let newNotes = Array.from(state.notes, (note) => {
-      return (note.id !== action.id)
-        ? note
-        : {...note, body: action.newBody}
-    })
+      return { ...state, notes: newNotes}
+    }
+    case 'NOTE_TITLE_CHANGED': {
+      let newNotes = Array.from(state.notes, (note) => {
+        return (note.id !== action.id)
+          ? note
+          : {...note, title: action.newTitle}
+      })
 
-    return { ...state, notes: newNotes}
-  }
-  case 'NOTE_TITLE_CHANGED': {
-    let newNotes = Array.from(state.notes, (note) => {
-      return (note.id !== action.id)
-        ? note
-        : {...note, title: action.newTitle}
-    })
-
-    return { ...state, notes: newNotes}
-  }
-  default:
-    return state
+      return { ...state, notes: newNotes}
+    }
+    default:
+      return state
   }
 }
